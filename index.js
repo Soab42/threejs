d8618b2e276688500659a789d89d35ff359f772a
Soab42 <108150913+Soab42@users.noreply.github.com>
2024-06-17 22:34:33 +0600

    update play

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 const audio = new Audio("./audio/joyful.mp3");
-audio.play();
+
+// Attempt to autoplay
+audio.play().catch(function (error) {
+  console.warn("Autoplay failed:", error);
+
+  // Setup interaction listeners to play audio
+  const playAudio = () => {
+    audio.play().catch(function (playError) {
+      console.error(
+        "Error attempting to play audio after user interaction:",
+        playError
+      );
+    });
+
+    // Remove the event listeners after the first interaction
+    document.removeEventListener("click", playAudio);
+    document.removeEventListener("keydown", playAudio);
+  };
+
+  document.addEventListener("click", playAudio);
+  document.addEventListener("keydown", playAudio);
+});
